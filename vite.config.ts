3e2c9118a93706ae14c276ebc0f6ea48428e0d6f
Onechance12 <120402543+Onechance12@users.noreply.github.com>
2026-07-28 13:13:24 -0600

    Remove unused Vite configuration

--- vite.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { sites } from "vinext/vite";
-
-export default defineConfig({ plugins: [sites(), cloudflare()] });
